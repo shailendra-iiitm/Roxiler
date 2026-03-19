b4eff1b5201d79dfc9f13da88b6a7d90d8984f21
Shailendra Shukla <shailendraiiits@gmail.com>
2026-03-19 16:17:03 +0530

    feat: implemented store listing with average rating, user-specific rating, search, and sorting

--- backend/controllers/storeController.js
+++ backend/controllers/storeController.js
@@ -1,4 +1,3 @@
-// src/controllers/storeController.js
 import db from "../config/db.js";
 
 export const createStore = (req, res) => {
@@ -13,5 +12,46 @@ export const createStore = (req, res) => {
     if (err) return res.status(500).json(err);
 
     res.json({ message: "Store created successfully" });
+  });
+};
+
+
+
+export const getStores = (req, res) => {
+  const user_id = req.user.id;
+  const { search, sortBy = "name", order = "ASC" } = req.query;
+
+  let sql = `
+    SELECT 
+      s.id,
+      s.name,
+      s.address,
+      IFNULL(AVG(r.rating), 0) AS overall_rating,
+      MAX(CASE WHEN r.user_id = ? THEN r.rating END) AS user_rating
+    FROM stores s
+    LEFT JOIN ratings r ON s.id = r.store_id
+  `;
+
+  const params = [user_id];
+
+
+  if (search) {
+    sql += ` WHERE s.name LIKE ? OR s.address LIKE ?`;
+    params.push(`%${search}%`, `%${search}%`);
+  }
+
+  sql += ` GROUP BY s.id`;
+
+  const allowedSort = ["name", "address", "overall_rating"];
+  const allowedOrder = ["ASC", "DESC"];
+
+  if (allowedSort.includes(sortBy) && allowedOrder.includes(order.toUpperCase())) {
+    sql += ` ORDER BY ${sortBy} ${order}`;
+  }
+
+  db.query(sql, params, (err, results) => {
+    if (err) return res.status(500).json(err);
+
+    res.json(results);
   });
 };
--- backend/routes/storeRoutes.js
+++ backend/routes/storeRoutes.js
@@ -2,10 +2,10 @@
 import express from "express";
 import { createStore } from "../controllers/storeController.js";
 import { verifyToken, requireRole } from "../middlewares/auth.js";
-
+import { getStores } from "../controllers/storeController.js";
 const router = express.Router();
 
 // Only ADMIN can create store
 router.post("/", verifyToken, requireRole("ADMIN"), createStore);
-
+router.get("/",verifyToken, getStores);
 export default router;
